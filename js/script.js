7b8c61c76fb9560f662d9d791858368043714fe0
JohnNino <jninocalde@gmail.com>
2022-05-02 12:42:43 -0500

    Normilize.css downloaded from TH
    This is the code I downloaded from Treehouse

---
+++ js/script.js
@@ -0,0 +1,60 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+const quotes = [
+  {
+      quote: "The greatest glory in living lies not in never falling, but in rising every time we fall",
+      source: "Nelson Mandela"
+
+  },
+  {
+      quote: "The way to get started is to quit talking and begin doing.",
+      source: "Walt Disney"
+
+  },
+  {
+      quote: "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking",
+      source: "Steve Jobs"
+
+  },
+  {
+      quote: "If life were predictable it would cease to be life, and be without flavor.",
+      source: "Eleanor Roosevelt"
+
+  },
+  {
+      quote: "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success",
+      source: "James Cameron"
+
+  },
+];
+
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
